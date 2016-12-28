94c8a3f5fe092e832b9a11bec952c0d02bb6527f
Willian Ribeiro <willianribeiro.contato@gmail.com>
2016-12-28 21:34:14 -0200

    Add a React component.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 'use strict'
 
-function sum(a, b) {
-  return a + b;
-}
+var React = require('react');
 
-module.exports = sum;
+var h1 = React.createElement('h1', null, 'Título do meu h1 - Hello World!');
+var Title = React.createClass({
+  render: function() {
+    return h1;
+  }
+});
+
+module.exports = Title;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 'use strict'
 
-var sum = require('./app');
-console.log(sum(1, 2));
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Title = require('./app');
+
+ReactDOM.render(
+  React.createElement(Title),
+  document.querySelector('[data-js="app"]')
+);

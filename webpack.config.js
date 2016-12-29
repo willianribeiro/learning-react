1cd4cea2b4d6b5526f932ec0ebac01a453db69bb
Willian Ribeiro <willianribeiro.contato@gmail.com>
2016-12-29 18:54:30 -0200

    Set configuration to use Standard js lint.

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,12 @@ module.exports = {
   ],
 
   module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'standard'
+    }],
     loaders : [{
       test: /\.js$/,
       exclude: /node_modules/,

f397e7e5001a4d25ad25c254069fc71874a7d261
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-01-14 00:02:38 -0200

    Learning method shouldComponentUpdate()

--- src/timer.js
+++ src/timer.js
@@ -3,6 +3,7 @@
 import React from 'react'
 
 class Timer extends React.Component {
+  // Executa quando o componente é instanciado.
   constructor () {
     super()
     this.state = {
@@ -10,21 +11,46 @@ class Timer extends React.Component {
     }
   }
 
+  // Executa antes do componente ser montado.
+  // Significa que este método é executado antes
+  // do elemento ser adicionado ao DOM real.
+  // (Ele pode ser executado no server side)
+  componentWillMount () {
+
+  }
+
+  // Este método é executado quando o componente
+  // já foi adicionado ao DOM real
   componentDidMount () {
     this.handleInterval = setInterval(() => {
       this.setState({time: this.state.time + 1})
     }, 1000)
   }
 
+  // Executa antes do componente ser removido do DOM
   componentWillUnmount () {
     clearInterval(this.handleInterval)
   }
 
+  // O React percebe que o objeto props foi modificado e
+  // antes de efetuar a modificacao ele executa este método
   componentWillReceiveProps (nextProps) {
     console.log('componentWillReceiveProps', this.props, nextProps)
   }
 
+  // Este método é executado antes do componente ser renderizado novamente
+  // pela chamada do método render(). Este método retorna true ou false,
+  // sendo que, true significa que o método render() será executado e
+  // false significa que não.
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('shouldComponentUpdate', nextProps, nextState)
+    return true
+  }
+
+  // Este método é chamado toda vez que há alguma alteração no state ou
+  // no props do componente
   render () {
+    console.log('render')
     return (
       <div>
         Timer: { this.state.time }

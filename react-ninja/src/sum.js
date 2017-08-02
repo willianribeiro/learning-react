db39fea030c1b9d488c5c02e0b9777bb722c3fd5
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-08-02 18:04:53 -0300

    [react-ninja] Add sum.js and sum.test.js

---
+++ react-ninja/src/sum.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const sum = () => {
+
+}
+
+export default sum;
---
+++ react-ninja/src/sum.test.js
@@ -0,0 +1,8 @@
+'use strict'
+
+import { expect } from 'chai';
+import sum from './sum';
+
+it('sum should be a function', () => {
+    expect(sum).to.be.a('function');
+});

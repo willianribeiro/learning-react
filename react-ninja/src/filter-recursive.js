6cccb0ec4f37d5bb8be23a08411382abbddb8949
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-08-15 17:30:55 -0300

    Implement code for all tests in filter-recursive

--- react-ninja/src/filter-recursive.js
+++ react-ninja/src/filter-recursive.js
@@ -1,14 +1,23 @@
 'use strict'
 
-const filter = (arr = [], func) => {
-    if (arr.length === 0)
-        return [];
-    
-    const [head, ...tail] = arr;
-    return [
-        func(head),
-        ...filter(tail, func)
-    ];
+const filter = (arr = [], func = item => item) => {
+    let counter = 0;
+
+    const internalFilter = (arr2) => {
+        if (arr2.length === 0)
+            return [];
+
+        const [head, ...tail] = arr2;
+
+        return func(head, counter, arr) ? [
+            head,
+            ...internalFilter(tail, counter++)
+        ] : [
+            ...internalFilter(tail, counter++)
+        ];
+    }
+
+    return internalFilter(arr, counter);
 }
 
 export default filter;

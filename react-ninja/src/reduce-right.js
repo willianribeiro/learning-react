70ad509ab9773f4239944a9804e82d05e857bc2b
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-08-22 18:04:04 -0300

    Add reduce right implementation (WIP)

--- react-ninja/src/reduce-right.js
+++ react-ninja/src/reduce-right.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const reduceRight = (arr, func, initialValue) => {
+    let acc = initialValue;
+    let arr2 = arr;
+    let lastIndex = arr.length - 1;
+
+    if (initialValue === undefined) {
+        acc = arr[lastIndex];
+        arr2 = arr.slice(0, lastIndex);
+        lastIndex = lastIndex - 1;
+    }
+
+    for (let i = lastIndex; i >= 0; i--)
+        acc = func(acc, arr2[i], i, arr);
+
+    return acc;
+}
+
+export default reduceRight;
--- react-ninja/src/reduce-right.test.js
+++ react-ninja/src/reduce-right.test.js
@@ -6,3 +6,49 @@ import reduceRight from './reduce-right';
 it('should be a function', () => {
     expect(reduceRight).to.be.a('function');
 });
+
+it('reduceRight([1, 2, 3], (acc, item) => acc + item, 0) should return 6', () => {
+    const before = reduceRight([1, 2, 3], (acc, item) => acc + item, 0);
+    const after = 6;
+    expect(before).to.be.equal(after);
+});
+
+it('reduceRight([2, 4, 6], (acc, item) => acc + item, 0) should return 12', () => {
+    const before = reduceRight([2, 4, 6], (acc, item) => acc + item, 0);
+    const after = 12;
+    expect(before).to.be.equal(after);
+});
+
+it('reduceRight([2, 4, 6], (acc, item) => acc + item, 10) should return 22', () => {
+    const before = reduceRight([2, 4, 6], (acc, item) => acc + item, 10);
+    const after = 22;
+    expect(before).to.be.equal(after);
+});
+
+it('reduceRight([2, 4, 6], (acc, item) => acc + item) should return 22', () => {
+    const before = reduceRight([2, 4, 6], (acc, item) => acc + item);
+    const after = 12;
+    expect(before).to.be.equal(after);
+});
+
+it('reduceRight([1, 1, 1], (acc, item, index) => acc + item + index) should return 4', () => {
+    const before = reduceRight([1, 1, 1], (acc, item, index) => acc + item + index);
+    const after = 4;
+    expect(before).to.be.equal(after);
+});
+
+it('reduceRight([1, 1, 1], (acc, item, index) => acc + item + index, 0) should return 6', () => {
+    const before = reduceRight([1, 1, 1], (acc, item, index) => acc + item + index, 0);
+    const after = 6;
+    expect(before).to.be.equal(after);
+});
+
+it('reduceRight([1, 1, 1], (acc, item, index, arr) => acc + arr[item]) should return 3', () => {
+    const before = reduceRight([1, 1, 1], (acc, item, index, arr) => acc + arr[item], 0);
+    const after = 3;
+    expect(before).to.be.equal(after);
+});
+
+// it('', () => {
+
+// });

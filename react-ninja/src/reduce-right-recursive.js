e96562e77e7c1604f839e0b94e4506fdb6a7b6f4
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-08-24 13:44:04 -0300

    Finish implementation of reduce right recursive

--- react-ninja/src/reduce-right-recursive.js
+++ react-ninja/src/reduce-right-recursive.js
@@ -2,29 +2,23 @@
 
 import reverse from './reverse';
 
-const reduceRight = (arr, func, initialValue) => {
-    let acc = initialValue;
-    let arrCopy = reverse(arr);
-    let lastIndex = arr.length - 1;
+const isUndefined = (value) => value === undefined;
 
-    if (initialValue === undefined) {
-        acc = arr[lastIndex];
-        arrCopy = reverse(arr).slice(0, lastIndex);
-        lastIndex = lastIndex - 1;
-    }
+const reduceRight = (arr, func, initialValue) => {
+    const arrReversed= reverse(arr);
+    const [head, ...tail] = arrReversed;
+    const acc = isUndefined(initialValue) ? head : initialValue;
+    const arrToIterate = isUndefined(initialValue) ? tail : arrReversed ;
 
     return (function reduceRightInternal(accInternal, arrInternal, counter) {
-        // for (let i = lastIndex; i >= 0; i--)
-        //     acc = func(acc, arrCopy[i], i, arr);
         const [head, ...tail] = arrInternal;
-        const nextAcc = func(accInternal, head, counter, arr);
+        const nextAcc = () => func(accInternal, head, counter, arr);
 
-        arrInternal.length === 0
+        return arrInternal.length === 0
             ? accInternal
-            : reduceRightInternal(nextAcc, tail, counter + 1);
-    
-        return acc;
-    })(acc, arrCopy, 0);
+            : reduceRightInternal(nextAcc(), tail, counter + 1);
+
+    })(acc, arrToIterate, 0);
 }
 
 export default reduceRight;

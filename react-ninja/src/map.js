5ff639114fadf079a351a6ea2a9cb116d86af04c
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-08-25 11:40:27 -0300

    Add tests to treat exception cases in map

--- react-ninja/src/map.js
+++ react-ninja/src/map.js
@@ -1,6 +1,12 @@
 'use strict'
 
 const map = (arr = [], func = (arr) => arr) => {
+  if (!Array.isArray(arr))
+    throw new TypeError('First parameter must be an array');
+
+  if (typeof func !== 'function')
+    throw new TypeError('Second parameter must be a function');
+
   let output = [];
 
   for (let i = 0; i < arr.length; i++) {
--- react-ninja/src/map.test.js
+++ react-ninja/src/map.test.js
@@ -20,14 +20,59 @@ it('map([1,2], (item, index, arr) => arr) should return [[1,2],[1,2]]', () => {
     .to.be.deep.equals([[1,2],[1,2]]);
 });
 
+it('map() should return []', () => {
+  expect(map()).to.be.deep.equals([]);
+});
+
 it('map([1,2]) should return [1,2]', () => {
   expect(map([1,2])).to.be.deep.equals([1,2]);
 });
 
-it('map([]) should return []', () => {
-  expect(map([])).to.be.deep.equals([]);
+it('map(1) should throw TypeError', () => {
+  let error;
+
+  try {
+    map(1);
+  } catch (e) {
+    error = e
+  }
+
+  expect(error.name).to.be.equal('TypeError');
 });
 
-it('map() should return []', () => {
-  expect(map()).to.be.deep.equals([]);
+it('map(1) should throw TypeError with message: "First parameter must be an array"', () => {
+  let error;
+
+  try {
+    map(1);
+  } catch (e) {
+    error = e
+  }
+
+  expect(error.message).to.be.equal('First parameter must be an array');
+});
+
+
+it('map([1, 2], "function") should throw TypeError"', () => {
+  let error;
+
+  try {
+    map([1, 2], 'function');
+  } catch (e) {
+    error = e
+  }
+
+  expect(error.name).to.be.equal('TypeError');
+});
+
+it('map([1, 2], "function") should throw TypeError with message: "Second parameter must be a function"', () => {
+  let error;
+
+  try {
+    map([1, 2], 'function');
+  } catch (e) {
+    error = e
+  }
+
+  expect(error.message).to.be.equal('Second parameter must be a function');
 });

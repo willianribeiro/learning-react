2c9898b8f09b935955bbc1b92df3a2c3a117089b
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-09-04 15:22:15 -0300

    Treat TypeError exceptions at div.js

--- react-ninja/module2/tdd/src/div.test.js
+++ react-ninja/module2/tdd/src/div.test.js
@@ -14,3 +14,51 @@ it('div(10, 5) should return 2', () => {
 it('div(30, 5) should return 6', () => {
     expect(div(30, 5)).to.be.equal(6);
 });
+
+it('div("10", 2) should throw TypeError', () => {
+    let error;
+
+    try {
+        div('10', 2)
+    } catch (e) {
+        error = e;
+    }
+
+    expect(error.name).to.be.equal('TypeError');
+});
+
+it('div(10, "2") should throw TypeError', () => {
+    let error;
+
+    try {
+        div(10, '2')
+    } catch (e) {
+        error = e;
+    }
+
+    expect(error.name).to.be.equal('TypeError');
+});
+
+it('div("10", 2) should throw TypeError with message: Parameter "a" must be a Number', () => {
+    let error;
+
+    try {
+        div('10', 2)
+    } catch (e) {
+        error = e;
+    }
+
+    expect(error.message).to.be.equal('Parameter "a" must be a Number');
+});
+
+it('div(10, "2") should throw TypeError with message: Parameter "b" must be a Number', () => {
+    let error;
+
+    try {
+        div(10, '2')
+    } catch (e) {
+        error = e;
+    }
+
+    expect(error.message).to.be.equal('Parameter "b" must be a Number');
+});

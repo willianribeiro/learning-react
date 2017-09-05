438756a910608acb960e9630514d3eb994d39252
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-09-05 11:54:56 -0300

    Treat TypeError exceptions at sum

--- react-ninja/module2/tdd/src/sum.js
+++ react-ninja/module2/tdd/src/sum.js
@@ -1,6 +1,12 @@
 'use strict'
 
 const sum = (a, b) => {
+    if (typeof a !== 'number')
+        throw TypeError('Parameter "a" must be a Number');
+
+    if (typeof b !== 'number')
+        throw TypeError('Parameter "b" must be a Number');
+
     return a + b;
 }
 
--- react-ninja/module2/tdd/src/sum.test.js
+++ react-ninja/module2/tdd/src/sum.test.js
@@ -14,3 +14,54 @@ it('sum(1, 2) should return 3', () => {
 it('sum(2, 3) should return 5', () => {
     expect(sum(2, 3)).to.be.equal(5);
 });
+
+it('sum("2", 3) should throw an TypeError', () => {
+    let error;
+
+    try {
+        sum("2", 3);
+    } catch (e) {
+        error = e;
+    }
+
+    expect(error.name).to.be.equal('TypeError');
+});
+
+it('sum("2", 3) should throw an TypeError with message: Parameter "a" must be a Number', () => {
+    let error;
+
+    try {
+        sum("2", 3);
+    } catch (e) {
+        error = e;
+    }
+
+    expect(error.name).to.be.equal('TypeError');
+    expect(error.message).to.be.equal('Parameter "a" must be a Number');
+});
+
+
+it('sum(2, "3") should throw an TypeError', () => {
+    let error;
+
+    try {
+        sum(2, "3");
+    } catch (e) {
+        error = e;
+    }
+
+    expect(error.name).to.be.equal('TypeError');
+});
+
+it('sum(2, "3") should throw an TypeError with message: Parameter "b" must be a Number', () => {
+    let error;
+
+    try {
+        sum(2, "3");
+    } catch (e) {
+        error = e;
+    }
+
+    expect(error.name).to.be.equal('TypeError');
+    expect(error.message).to.be.equal('Parameter "b" must be a Number');
+});

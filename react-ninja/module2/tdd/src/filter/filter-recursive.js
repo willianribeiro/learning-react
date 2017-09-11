71df0bcf5ad6109ed208f66d45ae3c3b26416231
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-09-11 13:56:34 -0300

    Treat TypeError exceptions at filter-recursive

--- react-ninja/module2/tdd/src/filter/filter-recursive.js
+++ react-ninja/module2/tdd/src/filter/filter-recursive.js
@@ -1,6 +1,12 @@
 'use strict'
 
 const filter = (arr = [], func = item => item) => {
+    if (!Array.isArray(arr))
+        throw new TypeError('First parameter must be an array');
+
+    if (typeof func !== 'function')
+        throw new TypeError('Second parameter must be a function');
+
     return (function filterInternal (arrayInternal, counter) {
         const [head, ...tail] = arrayInternal;
 

ca957da5adb8cc2dd6177c9e5c1296cde4298b2e
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-09-11 14:05:54 -0300

    Treat TypeError exceptions at find

--- react-ninja/module2/tdd/src/find/find.test.js
+++ react-ninja/module2/tdd/src/find/find.test.js
@@ -43,3 +43,29 @@ it('find([1, 2, 3], (item, index, arr) => arr[index] === item) should return 1',
     const after = 1;
     expect(before).to.be.equal(after);
 });
+
+it('find("array", () => {}) should throw a TypeError with message: "First parameter must be an array"', () => {
+    let error;
+
+    try {
+        find("array", () => {});
+    } catch (e) {
+        error = e;
+    }
+
+    expect(error.name).to.be.equal('TypeError');
+    expect(error.message).to.be.equal('First parameter must be an array');
+});
+
+it('find([1, 2], "function") should throw a TypeError with message: "Second parameter must be a function"', () => {
+    let error;
+
+    try {
+        find([1, 2], "function");
+    } catch (e) {
+        error = e;
+    }
+
+    expect(error.name).to.be.equal('TypeError');
+    expect(error.message).to.be.equal('Second parameter must be a function');
+});

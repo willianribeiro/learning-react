a28604356c4315e6bd81302314dad4efd8b9a817
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-09-12 17:42:55 -0300

    Create redux store

--- react-ninja/module_03/redux/main.js
+++ react-ninja/module_03/redux/main.js
@@ -6,9 +6,23 @@ const counter = (state = 0, action) => {
             return state + 1;
 
         case 'DECREMENT':
-            return state -1;
+            return state - 1;
 
         default:
             return state;
     }
 };
+
+const { createStore } = Redux;
+const store = createStore(counter);
+
+// store initial state
+console.log(store.getState());
+
+store.subscribe(() => {
+    console.log('disparou uma ação!', store.getState());
+});
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
